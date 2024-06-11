779621f69be60dd57a392ece86335ae9a6a37bed
IlyaBulatov <ezgoichik@gmail.com>
2024-06-11 18:26:18 +0500

    Added shoot mechanics
    Projectiles and their:
    Speed
    Amount
    Lifetime
    Damage
    
    Destructible script from one of previous projects

--- Ball Blast/Assets/BallBlastSF/Scripts/CartInputControl.cs
+++ Ball Blast/Assets/BallBlastSF/Scripts/CartInputControl.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class CartInputControl : MonoBehaviour
 {
     [SerializeField] private Cart cart;
+    [SerializeField] private Turret turret;
 
     private void Update()
     {
         cart.SetMovementTarget(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+
+        if(Input.GetMouseButtonDown(0) == true)
+        {
+            turret.Fire();
+        }
     }
 }
