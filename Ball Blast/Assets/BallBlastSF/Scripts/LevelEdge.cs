80448813921f75a41c73a1b18d8e30779d6f9381
IlyaBulatov <ezgoichik@gmail.com>
2024-06-14 01:08:29 +0500

    Add Stone Prefab
    Undestructible
    IsTrigger
    Speed is customize

---
+++ Ball Blast/Assets/BallBlastSF/Scripts/LevelEdge.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EdgeType
+{
+    Bottom,
+    Right,
+    Left
+}
+public class LevelEdge : MonoBehaviour
+{
+    [SerializeField] private EdgeType type;
+
+    public EdgeType Type => type;
+}
